4dad15b9b1f19564945c72e97027fd7b95a2bc8e
vanzhiganov <vanzhiganov@ya.ru>
2019-10-28 23:46:44 +0300

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="RocketPaySDK",
+    version="0.0.2",
+    author="Vyacheslav Anzhiganov",
+    author_email="vanzhiganov@ya.ru",
+    packages=[
+        "RocketPaySDK"
+    ],
+    install_requires=[
+        "requests"
+    ]
+)
